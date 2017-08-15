9d3bf07bebbd90cfd22f21f94be31709ef7134ae
moral <nevercometrue@outlook.com>
2017-08-15 19:52:01 +0800

    database code（2017-08-15）

---
+++ ego-manager/ego-manager-mapper/src/main/java/edu/moral/mapper/TbItemCatMapper.java
@@ -0,0 +1,30 @@
+package edu.moral.mapper;
+
+import edu.moral.pojo.TbItemCat;
+import edu.moral.pojo.TbItemCatExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TbItemCatMapper {
+    int countByExample(TbItemCatExample example);
+
+    int deleteByExample(TbItemCatExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(TbItemCat record);
+
+    int insertSelective(TbItemCat record);
+
+    List<TbItemCat> selectByExample(TbItemCatExample example);
+
+    TbItemCat selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") TbItemCat record, @Param("example") TbItemCatExample example);
+
+    int updateByExample(@Param("record") TbItemCat record, @Param("example") TbItemCatExample example);
+
+    int updateByPrimaryKeySelective(TbItemCat record);
+
+    int updateByPrimaryKey(TbItemCat record);
+}
---
+++ ego-manager/ego-manager-mapper/src/main/java/edu/moral/mapper/TbItemParamMapper.java
@@ -0,0 +1,36 @@
+package edu.moral.mapper;
+
+import edu.moral.pojo.TbItemParam;
+import edu.moral.pojo.TbItemParamExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TbItemParamMapper {
+    int countByExample(TbItemParamExample example);
+
+    int deleteByExample(TbItemParamExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(TbItemParam record);
+
+    int insertSelective(TbItemParam record);
+
+    List<TbItemParam> selectByExampleWithBLOBs(TbItemParamExample example);
+
+    List<TbItemParam> selectByExample(TbItemParamExample example);
+
+    TbItemParam selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") TbItemParam record, @Param("example") TbItemParamExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") TbItemParam record, @Param("example") TbItemParamExample example);
+
+    int updateByExample(@Param("record") TbItemParam record, @Param("example") TbItemParamExample example);
+
+    int updateByPrimaryKeySelective(TbItemParam record);
+
+    int updateByPrimaryKeyWithBLOBs(TbItemParam record);
+
+    int updateByPrimaryKey(TbItemParam record);
+}
